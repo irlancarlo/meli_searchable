9bc9284d026f71bb998dd6a4f26e1d2c2af6ddb6
IrlanGomesPessoal <irlan.carlo@gmail.com>
2021-05-23 14:44:58 -0400

    configurando searchview

--- app/src/main/java/com/irlangomes/melisearchable/activity/MainActivity.java
+++ app/src/main/java/com/irlangomes/melisearchable/activity/MainActivity.java
@@ -1,16 +1,19 @@
 package com.irlangomes.melisearchable.activity;
 
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.widget.LinearLayout;
 
 import com.irlangomes.melisearchable.R;
 import com.irlangomes.melisearchable.adapter.ProductAdapter;
 import com.irlangomes.melisearchable.model.Product;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,13 +23,18 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerProduct;
     private List<Product> products = new ArrayList<>();
     private ProductAdapter productAdapter;
+    private MaterialSearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         recyclerProduct = findViewById(R.id.recyclerProduct);
+        searchView = findViewById(R.id.searchView);
 
 
         // Config recycler
@@ -36,9 +44,43 @@ public class MainActivity extends AppCompatActivity {
         recyclerProduct.setLayoutManager(new LinearLayoutManager(this));
         recyclerProduct.setAdapter(productAdapter);
 
+        //config searchView
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+
+            }
+        });
+
     }
 
-    public void generateProduct(){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem item = menu.findItem(R.id.menu_search);
+        searchView.setMenuItem(item);
+
+        return true;
+    }
+
+    public void generateProduct() {
         Product product = new Product();
         product.setPicture("mot1.jpg");
         product.setTitle("Motorola Moto G6 Play Bueno Negro Liberado");
